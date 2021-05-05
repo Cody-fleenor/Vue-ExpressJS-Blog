559b6eeecceb5330fb4ac7ab7fd84f6f6985de84
Cody-fleenor <codyafleenor@gmail.com>
2021-05-05 12:08:40 -0700

    First Commit
    First Commit

---
+++ vue-blog/src/components/MainBody.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-container fluid class="bv-example-row">
+        <b-row>
+            <b-col class="container" style="padding: 15px;" sm="12" md="7">
+                <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </b-col>
+            <b-col class="container" style="padding: 15px;" sm="12" md="5">
+                <b-card
+                    title="Card Title"
+                    img-src="https://picsum.photos/600/300/?image=25"
+                    img-alt="Image"
+                    img-top
+                    tag="article"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    </b-card-text>
+
+                    <b-button href="#" variant="primary">Go somewhere</b-button>
+                </b-card>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-blog/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Import Bootstrap and BootstrapVue CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available
+Vue.use(BootstrapVue)
+// install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
